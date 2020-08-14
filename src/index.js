ed28d17edb7968ca6769ed5bd18de6cc11ee4f29
Nik Olson <pizzascripters@gmail.com>
2020-08-13 21:18:11 -0500

    Basic header

--- src/index.js
+++ src/index.js
@@ -4,10 +4,39 @@ import './index.css';
 import Strategy from './scenes/Strategy';
 import Simulation from './scenes/Simulation';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { page: 'strategy' };
+  }
+
+  onClick(page) {
+    this.setState({ page });
+  }
+
+  render() {
+    let content;
+    switch(this.state.page) {
+      case 'strategy':
+        content = <Strategy />;
+        break;
+      case 'simulation':
+        content = <Simulation />;
+        break;
+    }
+    return <div id='container'>
+      <header>
+        <a onClick={this.onClick.bind(this, 'strategy')}>Strategy</a>
+        <a onClick={this.onClick.bind(this, 'simulation')}>Simulation</a>
+      </header>
+      {content}
+    </div>;
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet" />
-    <Simulation />
+    <App />
   </React.StrictMode>,
   document.getElementById('root-container')
 );
